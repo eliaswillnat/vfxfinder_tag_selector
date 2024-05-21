0b32c0486edab9a0ca2fc9b17ba3e079a37b96e5
Elias Willnat <elias.willnat@gmail.com>
2024-05-21 14:30:13 +0200

    Added, Modified and Removed Tags

--- script.js
+++ script.js
@@ -5,30 +5,64 @@ document.addEventListener("DOMContentLoaded", () => {
             {
                 "name": "Elemental",
                 "tags": ["burn", "embers", "fire", "flame", "heat", "ignition", "pyro", "sparks", "lava", "magma", "cascade", "current",
-                        "flood", "ocean", "rain", "stream", "wave", "watersplash", "ice", "snow", "underwater", "gusts", "shockwave", "rock", "debris", "dust",
-                        "cascade", "current", "flood", "ocean", "rain", "splash", "stream", "wave"]
-            },
-            {
-                "name": "Supernatural",
-                "tags": ["dimensions", "darkdimension", "distortion", "magic", "spells", "superpower", "superhuman", "slowmotion"]
+                        "flood", "stream", "wave", "watersplash", "ice", "snow", "underwater", "gusts", "shockwave", "rock", "debris",
+                        "splash", "acidic", "electricity", "energy", "dust", "smoke",
+                        "liquid", "gas", "oxygen", "helium", "hydrogen", "rocks", "flames", "sand", "water", "droplet", "light", "plutonium", "air"]
             },
             {
                 "name": "Sci-Fi & Space",
-                "tags": ["spacetime", "galaxy", "blackhole", "wormhole", "hyperspace", "teleportation", "artificialintelligence", "aliens", "invasion",
-                        "ai", "clone", "mutation", "star", "nebula", "sun", "solarsystem", "propulsion", "warpdrive", "timetravel", "quantumteleportation", "atomic", "laser"]
+                "tags": ["spacetime", "blackhole", "wormhole", "hyperspace", "teleportation", "artificialintelligence", "aliens", "invasion",
+                        "ai", "clone", "mutation", "star", "nebula", "sun", "solarsystem", "propulsion", "atomic", "laser",
+                        "beam", "void", "moon", "solarwaves", "earth", "planet", "stars", "astronaut", "beyond", "city", "clones", "endless", "eternity",
+                        "scifi", "space", "futuristic", "galaxy", "universe", "kardashevscale1", "kardashevscale2", "kardashevscale3", "hud", "interstellar", "tachyondrive", "jumpdrive",
+                        "timetravel", "quantumteleportation", "warpspeed", "lasersword", "lightsaber", "laserbeam", "lightspeed", "warpdrive", "holography", "hologram", "microverse", "microworld",
+                        "nebula", "planetarysystem", "stellarsystem"]
             },
             {
                 "name": "Weather & Natural Phenomena",
-                "tags": ["storm", "thunderbolt", "earthquake", "eruption", "fissure", "volcano", "artificialintelligence", "ai", "clone",
-                        "mutation", "tornado", "twister", "mountain", "lightning"]
+                "tags": ["storm", "thunderbolt", "eruption", "fissure", "volcano", "clone",
+                        "mutation", "tornado", "twister", "mountain", "lightning", "rain", "coldness", "freezing", "snowing",
+                        "clouds", "windy", "sandstorm", "geyser", "tsunami", "thunder", "ocean", "heatwaves", "splash", "melting", "atmosphere",
+                        "earthquake", "night"]
+            },
+            {
+                "name": "Materials",
+                "tags": ["glass", "rubber", "fur", "wood", "plastics", "metals", "ceramics", "textiles", "concrete", "stones"]
             },
             {
                 "name": "Creatures, Magic & Fantasy",
-                "tags": ["dragon", "monster", "beast", "demon", "goblin", "troll", "vampire", "werewolf", "witch", "wizard", "elf", "dwarf", "orc", "dinosaurs", "trex"]
+                "tags": ["cells", "superpowers", "animals", "beasts", "bite", "bones", "creatures", "darkness", "magic", "utopian", "dystopian", "spells",
+                        "onodrim", "horde", "immortality", "invisible", "monsters", "superhuman", "paralyzed", "mutants", "reptiles", "saliva", "portals", "dragons",
+                         "beasts", "demons", "goblin", "trolls", "vampires", "werewolf", "witch", "wizards", "elf", "dwarf", "orcs", "dinosaurs", "trex"]
+            },
+            {
+                "name": "Physics",
+                "tags": ["absorption", "expansion", "spin", "force", "power", "radiation", "molecules", "particles", "relativity", "physics", "vibrations", "supersonic", "magneticfield",
+                         "soundwave", "acousticcavitation", "fluiddynamics", "chemicalreaction", "cymatics", "11d", "11th", "4d", "4th", "hyperspace", "accretiondisk", "mimicking", "atoms",
+                         "attraction", "bend", "brightness", "bubble", "electron", "darkmatter", "darkenergy", "dimensions", "distortion", "spacetime", "laser", "echo", "eleventh", "eventhorizon",
+                         "falling", "fourth", "fractal", "kaleidoscopic", "singularity", "nucleus", "glitch", "flight", "fly", "gravity", "higherdimension", "interdimensional", "hyperdrive",
+                         "hyperspeed", "infinity", "tachyon", "jump", "nanotechnology", "timedilation", "quantumrealm", "speedoflight", "timeloop", "timewarp", "xray", "ultrasonic", "propulsion",
+                         "timelapse", "microscopic", "slowmotion", "signal", "shrink"]
             },
             {
                 "name": "Destruction & Combat",
-                "tags": ["weapons", "swords", "guns", "bullets", "explosions", "implosion", "combat", "trails", "condensationcloud", "gunfire", "punches", "kicks", "fights", "wars", "battles", "destructions", "collapse", "fractures", "shatters"]
+                "tags": ["weapons", "swords", "guns", "bullets", "explosions", "implosions", "combat", "trails", "condensationcloud", "gunfire", "punches", "kicks", "fights", "wars", "battles",
+                         "collapse", "fractures", "shatters", "blasts", "blastrings", "shards", "burn", "atomicbomb", "rage", "eruption", "embers",
+                         "burst", "cracking", "collision", "disintegration", "debris", "volcano", "sparks", "massacre", "fissurevent", "shattered", "annihilation",
+                         "meteor", "wilsoncloud", "meteorite", "attacking", "army", "blood", "skindamage", "shooting", "kamikaze", "kick", "kill", "laserpointer", "breaking",
+                         "bulletime", "car", "chase", "chaos", "crowd", "dead", "death", "doom", "endoftheworld", "destructions", "armor", "injury", "grenade",
+                         "headshot", "impact", "infectionsinvade", "nanobots", "massmurder", "swarms", "nuclearattack", "holocaust", "armageddon", "supervillain", "wounds", "nuke", "military",
+                         "soldiers", "smash"]
+            },
+            {
+                "name": "Nature",
+                "tags": ["jungle", "forest", "tropical", "underwater", "ocean", "sea", "mountain", "mushroom", "tundra", "grassland", "desert", "trees"]
+            },
+            {
+                "name": "Vehicles & Machines",
+                "tags": ["cars", "bus", "airplanes", "rockets", "exosuits", "exoskeletons", "cyberneticsuits", "aircrafts", "bicycles", "trains", "flyingcars", "hovertrains", "maglevtrains",
+                         "driverless", "machines", "robots", "skateboard", "hoverboard", "hyperloop", "helicopter", "spaceship", "spacestation", "megastructures", "ringstation", "spaceelevator",
+                         "topopolis", "stellarengine", "matrioshkabrain", "dysonsphere", "gps"]
             }
         ]
     };
